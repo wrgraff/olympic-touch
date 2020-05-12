541c8bc5ff91019bfdc837a81306796b150ff0ee
wrgraff <wrgraff@gmail.com>
2020-05-12 21:42:06 +0300

    added tns slider to promo

---
+++ src/js/tiny-slider-settings.js
@@ -0,0 +1,20 @@
+const sliderList = document.querySelector('.slider__list');
+
+if (sliderList) {
+	const slider = tns({
+		container: sliderList,
+		controlsContainer: document.querySelector('.slider__controls'),
+		autoplayButton: document.querySelector('.slider__autoplay'),
+		autoplayText: ['<span class="visually-hidden">Старт</span>', '<span class="visually-hidden">Стоп</span>'],
+		nav: false,
+		items: 1,
+		autoplay: true,
+		autoWidth: true,
+		center: true,
+		responsive: {
+			1208: {
+				gutter: 4
+			}
+		}
+	});
+};
