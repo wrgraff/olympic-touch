095c0eb1a101f9740c013b4b59c03453924c0f27
wrgraff <wrgraff@gmail.com>
2020-05-18 01:58:21 +0300

    added ajax forms

--- src/js/form.js
+++ src/js/form.js
@@ -1,20 +1,19 @@
 (function() {
 	const form = document.querySelectorAll('.form');
-	const modalTemplate = document.querySelector('#modal').content.querySelector('#modal-window');
 
 	form.forEach(form => {
 		form.addEventListener('submit', (evt) => {
 			evt.preventDefault();
 
-			window.backend.sendForm(new FormData(form), 'http://localhost:3000' + form.getAttribute('action') + 'response.json', function(response) {
+			window.backend.sendForm(new FormData(form), form.getAttribute('action') + 'response.json', function(response) {
 				window.modal.show(createResponseText(response), true);
 			}, function(errorText) {
-				window.modal.show(createResponseText(response), true);
+				window.modal.show(errorText);
 			});
 		})
 	});
 
-	function createResponseText({status, heading, text, comment, contacts}) {
+	function createResponseText({status, heading, text, comment, contacts, error}) {
 		let responseContainer = window.utils.addElement('div', 'response');
 		responseContainer.classList.add('response_' + status);
 
@@ -28,13 +27,34 @@
 			let responseText = window.utils.addElement('div', 'response__text');
 			responseText.innerHTML = text;
 			responseContainer.append(responseText);
-		}
+		};
+
+		if (error) {
+			let responseDetails = window.utils.addElement('div', 'response__details');
+			let responseDetailsText = window.utils.addElement('p', 'response__title');
+			responseDetailsText.textContent = error.text;
+			responseDetails.append(responseDetailsText);
+
+			if (error.fields) {
+				let responseDetailsList = window.utils.addElement('ul');
+
+				error.fields.forEach((field, i, fields) => {
+					let responseDetailsItem = window.utils.addElement('li');
+					responseDetailsItem.textContent = (i + 1 < fields.length) ? field + ';' : field + '.';
+					responseDetailsList.append(responseDetailsItem);
+				});
+
+				responseDetails.append(responseDetailsList);
+			};
+
+			responseContainer.append(responseDetails);
+		};
 
 		if (comment) {
 			let responseComment = window.utils.addElement('p', 'response__comment');
 			responseComment.textContent = comment;
 			responseContainer.append(responseComment);
-		}
+		};
 
 		if (contacts) {
 			let responseContacts = window.utils.addElement('ul', 'response__contacts contacts');
@@ -58,12 +78,12 @@
 			responseContainer.append(responseContacts);
 
 			if (status == 'success') {
-				let responseButton = window.utils.addElement('a', 'response__button button button_secondary');
+				let responseButton = window.utils.addElement('a', 'response__button button');
 				responseButton.href = '/';
 				responseButton.textContent = 'Вернуться на главную';
 				responseContainer.append(responseButton);
 			} else {
-				let responseButton = window.utils.addElement('button', 'response__button button button_secondary');
+				let responseButton = window.utils.addElement('button', 'response__button button');
 				responseButton.type = 'button';
 				responseButton.dataset.micromodalClose = 'true';
 				responseButton.textContent = 'Вернуться к форме';
